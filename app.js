db68e18e1e09d89ef5b01b3267dd63da9ca92d7d
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-28 01:03:34 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+var cors = require('cors')
+const app = express();
+const path = require('path');
+const PORT = 8000;
+
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const conn = require('./middlewares/tidb');
+console.log("Trying to establish a connection to TiDB...");
+
+if (conn) {
+  console.log("Connection to TiDB successful");
+  var adminBroRouter = require('./routes/admin_bro');
+  //setting up admin bro
+  app.use('/admin', adminBroRouter)
+} else {
+  console.log("Connection to TiDB failed");
+}
+
+
+//Please don't delete this health API
+var usersRouter = require('./routes/user_routes');
+app.use('/api/health', (req, res) => {
+    res.send('Hello Autopilot');
+  });
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
